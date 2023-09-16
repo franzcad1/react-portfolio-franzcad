4155e914b993bf4b42a8c6a06697129907553747
Franz Cadiente <fcadien1@my.centennialcollege.ca>
2023-09-16 16:08:27 -0400

    reworked projects section

--- next.config.js
+++ next.config.js
@@ -1,6 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+  webpack: (config, options) => {
+    config.module.rules.push(
+      {
+        test: /\.pdf$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[ext]',
+              outputPath: 'public',
+            }
+          }
+        ],
+      });
+    return config;
+  },
+};
